7884cb27a03508392c73a3be473ab759824b707f
ats-12 <ats-12@ats12>
2018-05-18 14:49:00 +0530

    //Mahesh

--- src/main/java/com/ats/exhibitionfrontend/common/Constants.java
+++ src/main/java/com/ats/exhibitionfrontend/common/Constants.java
@@ -4,7 +4,9 @@ package com.ats.exhibitionfrontend.common;
 public class Constants {
 	
  
-	public static final String url="http://localhost:8093/";
-	//public static final String ReportURL = "http://localhost:8080/project/";
-	 
+	public static final String url="http://exhibition.aaryatechindia.in:12756/webapi/";
+	public static final int ITEM_IMAGE_TYPE =2;
+	public static final String PRODUCT_IMAGE = "http://exhibition.aaryatechindia.in:12756/uploads/PRODUCT/";
+	public static final String ITEM_IMAGE_URL = "http://exhibition.aaryatechindia.in:12756/uploads/ITEM/";
+
 }
