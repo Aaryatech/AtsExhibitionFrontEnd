4a2deab1fbaf0d24a4395dd15ec069edb24d3b43
ATS <ATS@192.168.2.5>
2018-06-01 11:02:24 +0530

    // NEHA

--- src/main/java/com/ats/exhibitionfrontend/controller/DashboardController.java
+++ src/main/java/com/ats/exhibitionfrontend/controller/DashboardController.java
@@ -30,11 +30,11 @@ import com.ats.exhibitionfrontend.model.LoginResponseExh;
 import com.ats.exhibitionfrontend.model.VisitorByExhiId;
 import com.ats.exhibitionfrontend.model.VisitorWithOrgEventName;
 import com.ats.exhibitionfrontend.model.eventhistory.GetEventVisitorName;
- 
+
 @Controller
 @Scope("session")
 public class DashboardController {
-	
+
 	List<EnquiryHeaderWithName> pending = new ArrayList<EnquiryHeaderWithName>();
 	List<EnquiryHeaderWithName> processing = new ArrayList<EnquiryHeaderWithName>();
 	List<EnquiryHeaderWithName> completed = new ArrayList<EnquiryHeaderWithName>();
@@ -47,125 +47,110 @@ public class DashboardController {
 	List<VisitorByExhiId> visitorList = new ArrayList<VisitorByExhiId>();
 	List<EnquiryHeaderWithName> enquiryList = new ArrayList<EnquiryHeaderWithName>();
 	List<FeedbackTxn> feedbackList = new ArrayList<FeedbackTxn>();
-	 
+
 	String fromDate = new String();
 	String toDate = new String();
-	
+
 	RestTemplate rest = new RestTemplate();
-	
+
 	@RequestMapping(value = "/exhibitorDashboard", method = RequestMethod.GET)
 	public ModelAndView addEmployee(HttpServletRequest request, HttpServletResponse response) {
 
 		ModelAndView model = new ModelAndView("dashboard");
-		try
-		{ 
-			
-			 
-			
-		}catch(Exception e)
-		{
+		try {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 		return model;
 	}
-	
-	@RequestMapping(value = "/getTotalList", method = RequestMethod.POST) 
+
+	@RequestMapping(value = "/getTotalList", method = RequestMethod.POST)
 	public ModelAndView getTotalList(HttpServletRequest request, HttpServletResponse response) {
 
 		ModelAndView model = new ModelAndView("dashboard");
-		
-		try
-		{ 
-			 fromDate = request.getParameter("fromDate");
-			 toDate = request.getParameter("toDate");
-			
-			HttpSession session = request.getSession(); 
+
+		try {
+			fromDate = request.getParameter("fromDate");
+			toDate = request.getParameter("toDate");
+
+			HttpSession session = request.getSession();
 			LoginResponseExh login = (LoginResponseExh) session.getAttribute("UserDetail");
-			
+
 			MultiValueMap<String, Object> map = new LinkedMultiValueMap<String, Object>();
 			map.add("exhId", login.getExhibitor().getExhId());
 			map.add("fromDate", DateConvertor.convertToYMD(fromDate));
 			map.add("toDate", DateConvertor.convertToYMD(toDate));
-			
+
 			EventExhMappingWithExhName[] res = rest.postForObject(Constants.url + "/eventListByExhIdBetweenDate", map,
-					EventExhMappingWithExhName[].class); 
-			 eventList = new ArrayList<EventExhMappingWithExhName>(Arrays.asList(res));
-			 
+					EventExhMappingWithExhName[].class);
+			eventList = new ArrayList<EventExhMappingWithExhName>(Arrays.asList(res));
+
 			VisitorByExhiId[] visitorByExhiId = rest.postForObject(Constants.url + "/visitorListByExhibitorId", map,
-					VisitorByExhiId[].class); 
-			 visitorList = new ArrayList<VisitorByExhiId>(Arrays.asList(visitorByExhiId));
-			
-			EnquiryHeaderWithName[] enquiryHeaderWithName = rest.postForObject(Constants.url + "/enquiryBetweenDateByExhibitorId", map,
-					EnquiryHeaderWithName[].class); 
-			 enquiryList = new ArrayList<EnquiryHeaderWithName>(Arrays.asList(enquiryHeaderWithName));
-			
+					VisitorByExhiId[].class);
+			visitorList = new ArrayList<VisitorByExhiId>(Arrays.asList(visitorByExhiId));
+
+			EnquiryHeaderWithName[] enquiryHeaderWithName = rest.postForObject(
+					Constants.url + "/enquiryBetweenDateByExhibitorId", map, EnquiryHeaderWithName[].class);
+			enquiryList = new ArrayList<EnquiryHeaderWithName>(Arrays.asList(enquiryHeaderWithName));
+
 			FeedbackTxn[] feedbackTxn = rest.postForObject(Constants.url + "/feedbackbetweenDate", map,
-					FeedbackTxn[].class); 
-			 feedbackList = new ArrayList<FeedbackTxn>(Arrays.asList(feedbackTxn));
-			
+					FeedbackTxn[].class);
+			feedbackList = new ArrayList<FeedbackTxn>(Arrays.asList(feedbackTxn));
+
 			map = new LinkedMultiValueMap<String, Object>();
 			map.add("exhId", login.getExhibitor().getExhId());
 			map.add("fromDate", DateConvertor.convertToYMD(fromDate));
 			map.add("toDate", DateConvertor.convertToYMD(toDate));
 			map.add("status", 4);
-			EnquiryHeaderWithName[] notBetweenClosed = rest.postForObject(Constants.url + "/enquiryNotBetweenDateWithStatus", map,
-					EnquiryHeaderWithName[].class); 
+			EnquiryHeaderWithName[] notBetweenClosed = rest.postForObject(
+					Constants.url + "/enquiryNotBetweenDateWithStatus", map, EnquiryHeaderWithName[].class);
 			enquiryNotBetweenClosedList = new ArrayList<EnquiryHeaderWithName>(Arrays.asList(notBetweenClosed));
-			
+
 			map = new LinkedMultiValueMap<String, Object>();
 			map.add("exhId", login.getExhibitor().getExhId());
 			map.add("fromDate", DateConvertor.convertToYMD(fromDate));
 			map.add("toDate", DateConvertor.convertToYMD(toDate));
 			map.add("status", 5);
-			EnquiryHeaderWithName[] notBetweenCompleted = rest.postForObject(Constants.url + "/enquiryNotBetweenDateWithStatus", map,
-					EnquiryHeaderWithName[].class); 
+			EnquiryHeaderWithName[] notBetweenCompleted = rest.postForObject(
+					Constants.url + "/enquiryNotBetweenDateWithStatus", map, EnquiryHeaderWithName[].class);
 			enquiryNotBetweenCompletedList = new ArrayList<EnquiryHeaderWithName>(Arrays.asList(notBetweenCompleted));
-			
+
 			map = new LinkedMultiValueMap<String, Object>();
-			map.add("exhId", login.getExhibitor().getExhId()); 
+			map.add("exhId", login.getExhibitor().getExhId());
 			map.add("status", 1);
 			EnquiryHeaderWithName[] allPending = rest.postForObject(Constants.url + "/equiryListWithStatus", map,
-					EnquiryHeaderWithName[].class); 
-			 allEnquiryPending = new ArrayList<EnquiryHeaderWithName>(Arrays.asList(allPending));
-			
+					EnquiryHeaderWithName[].class);
+			allEnquiryPending = new ArrayList<EnquiryHeaderWithName>(Arrays.asList(allPending));
+
 			map = new LinkedMultiValueMap<String, Object>();
-			map.add("exhId", login.getExhibitor().getExhId()); 
+			map.add("exhId", login.getExhibitor().getExhId());
 			map.add("status", 2);
 			EnquiryHeaderWithName[] allProcessing = rest.postForObject(Constants.url + "/equiryListWithStatus", map,
-					EnquiryHeaderWithName[].class); 
+					EnquiryHeaderWithName[].class);
 			allEnquiryProcessing = new ArrayList<EnquiryHeaderWithName>(Arrays.asList(allProcessing));
-			
+
 			pending = new ArrayList<EnquiryHeaderWithName>();
 			processing = new ArrayList<EnquiryHeaderWithName>();
 			completed = new ArrayList<EnquiryHeaderWithName>();
 			closed = new ArrayList<EnquiryHeaderWithName>();
-			
-			
-			for(int i=0;i<enquiryList.size();i++)
-			{
-				if(enquiryList.get(i).getStatus()==1)
-				{
+
+			for (int i = 0; i < enquiryList.size(); i++) {
+				if (enquiryList.get(i).getStatus() == 1) {
 					pending.add(enquiryList.get(i));
-				}
-				else if(enquiryList.get(i).getStatus()==2)
-				{
+				} else if (enquiryList.get(i).getStatus() == 2) {
 					processing.add(enquiryList.get(i));
-				}
-				else if(enquiryList.get(i).getStatus()==5)
-				{
+				} else if (enquiryList.get(i).getStatus() == 5) {
 					completed.add(enquiryList.get(i));
-				}
-				else if(enquiryList.get(i).getStatus()==4)
-				{
+				} else if (enquiryList.get(i).getStatus() == 4) {
 					closed.add(enquiryList.get(i));
 				}
 			}
-			 
-			 
+
 			model.addObject("fromDate", fromDate);
 			model.addObject("toDate", toDate);
-			
+
 			model.addObject("pending", pending);
 			model.addObject("processing", processing);
 			model.addObject("completed", completed);
@@ -174,134 +159,119 @@ public class DashboardController {
 			model.addObject("enquiryNotBetweenCompletedList", enquiryNotBetweenCompletedList);
 			model.addObject("allEnquiryProcessing", allEnquiryProcessing);
 			model.addObject("allEnquiryPending", allEnquiryPending);
-			   
+
 			model.addObject("eventList", eventList);
 			model.addObject("visitorList", visitorList);
 			model.addObject("enquiryList", enquiryList);
 			model.addObject("feedbackList", feedbackList);
-			
-			
-			
-			
-		}catch(Exception e)
-		{
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 		return model;
 	}
-	
+
 	@RequestMapping(value = "/getChart", method = RequestMethod.GET)
 	@ResponseBody
 	public ExhibitorDashboard getChart(HttpServletRequest request, HttpServletResponse response) {
 
-		 ExhibitorDashboard exhibitorDashboard = new ExhibitorDashboard();
-		try
-		{  
-			
-			 exhibitorDashboard.setTotalPending(pending.size());
-			 exhibitorDashboard.setTotalProcessing(processing.size());
-			 exhibitorDashboard.setTotalClosed(closed.size());
-			 exhibitorDashboard.setTotalCompleted(completed.size());
-			 
-			 
-		}catch(Exception e)
-		{
+		ExhibitorDashboard exhibitorDashboard = new ExhibitorDashboard();
+		try {
+
+			exhibitorDashboard.setTotalPending(pending.size());
+			exhibitorDashboard.setTotalProcessing(processing.size());
+			exhibitorDashboard.setTotalClosed(closed.size());
+			exhibitorDashboard.setTotalCompleted(completed.size());
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 		return exhibitorDashboard;
 	}
-	
-	
+
 	@RequestMapping(value = "/enquiryList/{status}", method = RequestMethod.GET)
-	public ModelAndView enquiryList(@PathVariable int status, HttpServletRequest request, HttpServletResponse response) {
+	public ModelAndView enquiryList(@PathVariable int status, HttpServletRequest request,
+			HttpServletResponse response) {
 
 		ModelAndView model = new ModelAndView("masters/enquiryList");
-		try
-		{ 
-			if(status==1)
-				 model.addObject("list", pending);
-			else if (status==2)
+		try {
+			if (status == 1)
+				model.addObject("list", pending);
+			else if (status == 2)
 				model.addObject("list", processing);
-			else if (status==4)
+			else if (status == 4)
 				model.addObject("list", closed);
-			else if (status==5)
+			else if (status == 5)
 				model.addObject("list", completed);
-			else if (status==6)
+			else if (status == 6)
 				model.addObject("list", enquiryNotBetweenClosedList);
-			else if (status==7)
+			else if (status == 7)
 				model.addObject("list", enquiryNotBetweenCompletedList);
-			else if (status==8)
+			else if (status == 8)
 				model.addObject("list", allEnquiryPending);
-			else if (status==9)
+			else if (status == 9)
 				model.addObject("list", allEnquiryProcessing);
-			
-			
-			
-			VisitorWithOrgEventName[] visNames = rest.getForObject(Constants.url + "/getAllVisitorsByIsUsed",  
+
+			VisitorWithOrgEventName[] visNames = rest.getForObject(Constants.url + "/getAllVisitorsByIsUsed",
 					VisitorWithOrgEventName[].class);
 
-			List<VisitorWithOrgEventName> visNameList = new ArrayList<VisitorWithOrgEventName>(Arrays.asList(visNames)); 
+			List<VisitorWithOrgEventName> visNameList = new ArrayList<VisitorWithOrgEventName>(Arrays.asList(visNames));
 			model.addObject("visNameList", visNameList);
 			model.addObject("status", status);
 			model.addObject("fromDate", fromDate);
 			model.addObject("toDate", toDate);
-			 
-		}catch(Exception e)
-		{
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 		return model;
 	}
-	
+
 	@RequestMapping(value = "/showVisitorLikesList", method = RequestMethod.GET)
 	public ModelAndView showVisitorLikesList(HttpServletRequest request, HttpServletResponse response) {
 
 		ModelAndView model = new ModelAndView("masters/visitorLikes");
-		try
-		{ 
-			
+		try {
+
 			model.addObject("fromDate", fromDate);
 			model.addObject("toDate", toDate);
-			
+
 			model.addObject("visitorList", visitorList);
-			
-		}catch(Exception e)
-		{
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 		return model;
 	}
-	 
+
 	@RequestMapping(value = "/visitorLikesList", method = RequestMethod.GET)
 	@ResponseBody
 	public List<VisitorByExhiId> visitorLikesList(HttpServletRequest request, HttpServletResponse response) {
 
 		List<VisitorByExhiId> exhibitorDashboard = new ArrayList<VisitorByExhiId>();
-		try
-		{ 
+		try {
 			String fromDate = request.getParameter("fromDate");
 			String toDate = request.getParameter("toDate");
-			
-			HttpSession session = request.getSession(); 
+
+			HttpSession session = request.getSession();
 			LoginResponseExh login = (LoginResponseExh) session.getAttribute("UserDetail");
-			
+
 			MultiValueMap<String, Object> map = new LinkedMultiValueMap<String, Object>();
 			map.add("exhId", login.getExhibitor().getExhId());
 			map.add("fromDate", DateConvertor.convertToYMD(fromDate));
 			map.add("toDate", DateConvertor.convertToYMD(toDate));
-			
+
 			VisitorByExhiId[] visitorByExhiId = rest.postForObject(Constants.url + "/visitorListByExhibitorId", map,
-					VisitorByExhiId[].class); 
+					VisitorByExhiId[].class);
 			exhibitorDashboard = new ArrayList<VisitorByExhiId>(Arrays.asList(visitorByExhiId));
-			
+
 			System.out.println(exhibitorDashboard);
-			
-		}catch(Exception e)
-		{
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
--- src/main/java/com/ats/exhibitionfrontend/model/ExhEmpWithExhName.java
+++ src/main/java/com/ats/exhibitionfrontend/model/ExhEmpWithExhName.java
@@ -1,84 +1,122 @@
 package com.ats.exhibitionfrontend.model;
- 
+
 public class ExhEmpWithExhName {
-	 
-	private int empId; 
-	private int exhId; 
-	private String exhName; 
-	private String empName; 
-	private String empDesignation; 
-	private String empMobile; 
-	private String empEmail; 
-	private String empPhoto; 
-	private String empEducation; 
+
+	private int empId;
+	private int exhId;
+	private String exhName;
+	private String empName;
+	private String empDesignation;
+	private String empMobile;
+	private String empEmail;
+	private String empPhoto;
+	private String empEducation;
 	private int isUsed;
+	private String password;
+	private String token;
+
 	public int getEmpId() {
 		return empId;
 	}
+
 	public void setEmpId(int empId) {
 		this.empId = empId;
 	}
+
 	public int getExhId() {
 		return exhId;
 	}
+
 	public void setExhId(int exhId) {
 		this.exhId = exhId;
 	}
+
 	public String getExhName() {
 		return exhName;
 	}
+
 	public void setExhName(String exhName) {
 		this.exhName = exhName;
 	}
+
 	public String getEmpName() {
 		return empName;
 	}
+
 	public void setEmpName(String empName) {
 		this.empName = empName;
 	}
+
 	public String getEmpDesignation() {
 		return empDesignation;
 	}
+
 	public void setEmpDesignation(String empDesignation) {
 		this.empDesignation = empDesignation;
 	}
+
 	public String getEmpMobile() {
 		return empMobile;
 	}
+
 	public void setEmpMobile(String empMobile) {
 		this.empMobile = empMobile;
 	}
+
 	public String getEmpEmail() {
 		return empEmail;
 	}
+
 	public void setEmpEmail(String empEmail) {
 		this.empEmail = empEmail;
 	}
+
 	public String getEmpPhoto() {
 		return empPhoto;
 	}
+
 	public void setEmpPhoto(String empPhoto) {
 		this.empPhoto = empPhoto;
 	}
+
 	public String getEmpEducation() {
 		return empEducation;
 	}
+
 	public void setEmpEducation(String empEducation) {
 		this.empEducation = empEducation;
 	}
+
 	public int getIsUsed() {
 		return isUsed;
 	}
+
 	public void setIsUsed(int isUsed) {
 		this.isUsed = isUsed;
 	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
 	@Override
 	public String toString() {
 		return "ExhEmpWithExhName [empId=" + empId + ", exhId=" + exhId + ", exhName=" + exhName + ", empName="
 				+ empName + ", empDesignation=" + empDesignation + ", empMobile=" + empMobile + ", empEmail=" + empEmail
-				+ ", empPhoto=" + empPhoto + ", empEducation=" + empEducation + ", isUsed=" + isUsed + "]";
+				+ ", empPhoto=" + empPhoto + ", empEducation=" + empEducation + ", isUsed=" + isUsed + ", password="
+				+ password + ", token=" + token + "]";
 	}
-	
-	
 
 }
