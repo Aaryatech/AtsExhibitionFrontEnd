f1793aa8a0c965c9c9e67970f406e884cc40ab2e
Aaryatech <code@atsinfosoft.com>
2018-06-13 10:46:37 +0530

    cons//Sac

--- src/main/java/com/ats/exhibitionfrontend/common/Constants.java
+++ src/main/java/com/ats/exhibitionfrontend/common/Constants.java
@@ -4,9 +4,19 @@ package com.ats.exhibitionfrontend.common;
 public class Constants {
 	
  
-	public static final String url="http://exhibition.aaryatechindia.in:12756/webapi/";
+	public static final String url="http://localhost:8094/";
+//	public static final String url="http://exhibition.aaryatechindia.in:12756/webapi/";
+
 	public static final int ITEM_IMAGE_TYPE =2;
 	public static final String PRODUCT_IMAGE = "http://exhibition.aaryatechindia.in:12756/uploads/PRODUCT/";
 	public static final String ITEM_IMAGE_URL = "http://exhibition.aaryatechindia.in:12756/uploads/ITEM/";
+	public static String REPORT_SAVE;
+	
+	public static final String EVENT_IMG_URL = "http://exhibition.aaryatechindia.in:12756/uploads/EVENT/";
+	public static final String MEMBER_IMG = "http://exhibition.aaryatechindia.in:12756/uploads/MEMBER/";
+	public static final String SPONSOR_IMG = "http://exhibition.aaryatechindia.in:12756/uploads/SPONSOR/";
 
+
+	
+	
 }
